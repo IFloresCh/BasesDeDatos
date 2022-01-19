82d345a1c02cf8015298c6324129d71ab5ba936a
IFloresCh <94060293+IFloresCh@users.noreply.github.com>
2022-01-19 19:38:03 +0100

    Update CreateDB.sql

--- Tasca1/CreateDB.sql
+++ Tasca1/CreateDB.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE espana;
 use espana;
 CREATE TABLE COMUNITAT (
-    Codi_comunitat VARCHAR(10) NOT NULL,
+    Codi_comunitat INT NOT NULL,
     Nom_comunitat VARCHAR(50)NOT NULL,
     PRIMARY KEY (Codi_comunitat)
 );
 CREATE TABLE PROVINCIA (
-    Codi_provincia VARCHAR(10) NOT NULL,
+    Codi_provincia INT NOT NULL,
     Nom_província VARCHAR(50)NOT NULL,
-    p_comunitat VARCHAR(10) ,
+    p_comunitat INT NOT NULL ,
     PRIMARY KEY(Codi_provincia),
     FOREIGN KEY (p_comunitat) REFERENCES COMUNITAT(Codi_comunitat)
 );
 CREATE TABLE MUNICIPIS (
-    Codi_municipi VARCHAR(10) NOT NULL,
+    Codi_municipi INT NOT NULL,
     Nom VARCHAR(50) NOT NULL,
-    p_provincia VARCHAR(10),
+    p_provincia INT NOT NULL,
     PRIMARY KEY(Codi_municipi),
     FOREIGN KEY (p_provincia) REFERENCES PROVINCIA(Codi_provincia)
 );
 CREATE TABLE EMPLEAT (
-    Nom_E VARCHAR(100) NOT NULL,
+    Nom_E INT NOT NULL,
     DNI_E VARCHAR(25) NOT NULL,
     Nom VARCHAR(100),
     Telèfon VARCHAR(25),
